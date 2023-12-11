dab654aca1385e767b30ff960bb00cb48281a29e
Sascha-T <info@sascha-t.de>
2023-12-11 14:03:52 +0000

    first ci stuff

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+TAG=`cat TAGNAME.github`
+mkdir work
+
+URL=`curl https://api.github.com/repos/packwiz/packwiz-installer-bootstrap/releases/latest | jq '.["assets"][0]["browser_download_url"]' | tr -d '"'`
+wget ${URL}
+
+mv packwiz-installer-bootstrap.jar work/
+cp mmc-pack.json work/
+cp icon.png work/
+sed "s@%TAGNAME%@${TAG}@g" instance.cfg > work/instance.cfg
+cd work; zip -D -r -q ../moondial.zip *
