a851d8539ee781081e05df7ffa194d7f94649dd1
Sascha T <info@sascha-t.de>
2023-12-11 15:58:58 +0100

    fix: part1

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 TAG=`cat TAGNAME.github`
-mkdir work
+mkdir -p work/.minecraft
 
 URL=`curl https://api.github.com/repos/packwiz/packwiz-installer-bootstrap/releases/latest | jq '.["assets"][0]["browser_download_url"]' | tr -d '"'`
 wget ${URL}
 
-mv packwiz-installer-bootstrap.jar work/
+mv packwiz-installer-bootstrap.jar work/.minecraft/
 cp mmc-pack.json work/
 cp icon.png work/
 sed "s@%TAGNAME%@${TAG}@g" instance.cfg > work/instance.cfg
